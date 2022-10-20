13e3b9405b9b115f601b4ed5dfa77eea9867255d
elliothros <elliothros@gmail.com>
2022-10-20 13:00:07 +0200

    Set up table

--- my-app/src/app/product-list/product-list.component.ts
+++ my-app/src/app/product-list/product-list.component.ts
@@ -8,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class ProductListComponent implements OnInit {
 
   title: string = "Product Management System";
+  products: any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getTitle(): string {
+    return this.title;
   }
 
 }
